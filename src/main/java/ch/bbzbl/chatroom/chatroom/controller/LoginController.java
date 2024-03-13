b14fa915c47f4e362c8088641c4d2741dd8ccf5f
Enrico Proietto <eproietto03@gmail.com>
2024-03-13 14:28:48 +0100

    moe secure login

--- src/main/java/ch/bbzbl/chatroom/chatroom/controller/LoginController.java
+++ src/main/java/ch/bbzbl/chatroom/chatroom/controller/LoginController.java
@@ -1,9 +1,11 @@
 package ch.bbzbl.chatroom.chatroom.controller;
 
 import ch.bbzbl.chatroom.chatroom.model.user.Users;
+import ch.bbzbl.chatroom.chatroom.security.SecurityConfig;
 import ch.bbzbl.chatroom.chatroom.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,24 +15,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class LoginController {
-    @Autowired
-    private UserService service;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Autowired
-    private UserController userController;
-    private SessionController sessionController;
+    private final UserService service;
+    private final SecurityConfig securityConfig;
+	private final SessionController sessionController;
 
-    @GetMapping("/login")
+	public LoginController(UserService service, UserController userController, SecurityConfig securityConfig, SessionController sessionController) {
+		this.service = service;
+		this.securityConfig = securityConfig;
+		this.sessionController = sessionController;
+	}
+
+	@GetMapping("/login")
     public String showLoginForm(Model model) {
         model.addAttribute("user", new Users());
         return "login";
     }
 
-    @PostMapping
+    @PostMapping("/loginUser")
     public String processLogin(Users user) {
         Users userFromDb = service.findByEmail(user.getEmail());
+        PasswordEncoder passwordEncoder = securityConfig.passwordEncoder();
 
-        if (bCryptPasswordEncoder.matches(user.getPassword(), userFromDb.getPassword())) {
+        if (passwordEncoder.matches(user.getPassword(), userFromDb.getPassword())) {
             sessionController.setSession(user);
             return "redirect:/chat";
         }
