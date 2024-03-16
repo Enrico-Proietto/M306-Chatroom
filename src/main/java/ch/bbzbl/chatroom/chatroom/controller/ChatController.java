f0e0f55865a27d7f511dc0f7f129bf8a4bd1f8cd
blochh <ilona.bloch@bbzbl-it.ch>
2024-03-16 18:28:40 +0100

    creatChat

--- src/main/java/ch/bbzbl/chatroom/chatroom/controller/ChatController.java
+++ src/main/java/ch/bbzbl/chatroom/chatroom/controller/ChatController.java
@@ -2,12 +2,15 @@ package ch.bbzbl.chatroom.chatroom.controller;
 
 import ch.bbzbl.chatroom.chatroom.model.chat.Chat;
 import ch.bbzbl.chatroom.chatroom.model.user.Users;
+import ch.bbzbl.chatroom.chatroom.service.ChatService;
 import ch.bbzbl.chatroom.chatroom.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.apache.catalina.User;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,10 +21,12 @@ public class ChatController {
 
     private final SessionController sessionController;
     private final UserService userService;
+    private final ChatService service;
 
-    public ChatController(SessionController sessionController, UserService userService) {
+    public ChatController(SessionController sessionController, UserService userService, ChatService service) {
         this.sessionController = sessionController;
         this.userService = userService;
+        this.service = service;
     }
 
     @GetMapping("/chat")
@@ -33,8 +38,25 @@ public class ChatController {
         return "chat";
     }
 
+    @GetMapping("/createChat")
+    public String showChatForm(Model model) {
+        model.addAttribute("chat", new Chat());
+        return "createChat";
+    }
+
+    @PostMapping("/create")
+    public String processChat(Chat chat) {
+
+
+        service.save(chat);
+        return "redirect:/chat";
+    }
+
     @GetMapping("/chatrooms")
     public void getChatRooms(Model model) {
 
     }
+
+
+
 }
