bb3816486ddc69b2c0f15be2dc9ae17e28bb73a4
Enrico-Proietto <enrico.proiettog@gmail.com>
2024-03-21 11:07:59 +0100

    combination of 2 branches

--- src/main/java/ch/bbzbl/chatroom/chatroom/controller/ChatController.java
+++ src/main/java/ch/bbzbl/chatroom/chatroom/controller/ChatController.java
@@ -1,62 +1,96 @@
 package ch.bbzbl.chatroom.chatroom.controller;
 
 import ch.bbzbl.chatroom.chatroom.model.chat.Chat;
+import ch.bbzbl.chatroom.chatroom.model.message.Message;
 import ch.bbzbl.chatroom.chatroom.model.user.Users;
 import ch.bbzbl.chatroom.chatroom.service.ChatService;
+import ch.bbzbl.chatroom.chatroom.service.MessageService;
 import ch.bbzbl.chatroom.chatroom.service.UserService;
+import com.sun.jdi.request.InvalidRequestStateException;
 import jakarta.servlet.http.HttpSession;
-import org.apache.catalina.User;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.lang.Nullable;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping
 public class ChatController {
 
-    private final SessionController sessionController;
-    private final UserService userService;
-    private final ChatService service;
+	private final SessionController sessionController;
+	private final UserService userService;
+	private final ChatService chatService;
+	private final MessageService messageService;
 
-    public ChatController(SessionController sessionController, UserService userService, ChatService service) {
-        this.sessionController = sessionController;
-        this.userService = userService;
-        this.service = service;
-    }
+	public ChatController(SessionController sessionController, UserService userService, ChatService chatService, MessageService messageService) {
+		this.sessionController = sessionController;
+		this.userService = userService;
+		this.chatService = chatService;
+		this.messageService = messageService;
+	}
 
-    @GetMapping("/chat")
-    public String showChat(Model model) {
-        HttpSession session = sessionController.getSession();
-        Object userID = session.getAttribute("userId");
-        Users user = userService.getById((Long) userID);
-        model.addAttribute("chatrooms", user.getChat());
-        return "chat";
-    }
+	@GetMapping("/chat")
+	public String showChat(@Nullable Long chatID, Model model) {
+		HttpSession session = sessionController.getSession();
+		Object userID = session.getAttribute("userId");
+		Users user = userService.getById((Long) userID);
 
-    @GetMapping("/createChat")
-    public String showChatForm(Model model) {
-        model.addAttribute("chat", new Chat());
-        return "createChat";
-    }
+		if (user != null) {
+			if (chatID != null) {
+				for (Chat chat : user.getChat()) {
+					if (Objects.equals(chat.getId(), chatID)) {
+						List<Users> listOfUser = userService.findAll();
+						listOfUser.removeAll(chatService.getById(chatID).getUser());
+						session.setAttribute("chatID", chatID);
+						session.setAttribute("user-username", user.getFirstname() + " " + user.getLastname());
+						model.addAttribute("userNotInChatroom", listOfUser);
+						model.addAttribute("userInChatroom", chatService.getById(chatID).getUser());
+						model.addAttribute("newMessage", new Message());
+						model.addAttribute("messages", messageService.findMessageByChatId(chatID));
+					}
+				}
+			}
 
-    @PostMapping("/create")
-    public String processChat(Chat chat) {
+			model.addAttribute("chatrooms", user.getChat());
+
+			return "chat";
+		}
+
+		return "redirect:/login";
+	}
+
+	@GetMapping("/createChat")
+	public String showChatForm(Model model) {
+		model.addAttribute("chat", new Chat());
+		return "createChat";
+	}
+
+	@PostMapping("/create")
+	public String processChat(Chat chat) {
 
 
-        service.save(chat);
-        return "redirect:/chat";
-    }
+		chatService.save(chat);
+		return "redirect:/chat";
+	}
 
-    @GetMapping("/chatrooms")
-    public void getChatRooms(Model model) {
+	@GetMapping("/chatrooms")
+	public void getChatRooms(Model model) {
 
-    }
+	}
 
-
-
+	/*
+	Invalidates the session of user and redirects to login page
+	 */
+	@GetMapping("/logout")
+	public String logout(Model model) {
+		sessionController.setSessionInvalid();
+		return "redirect:/login";
+	}
 }
